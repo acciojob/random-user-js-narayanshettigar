0c1793202fe7259023b0a2d360aab4bcbfde65e7
narayanshettigar <117894336+narayanshettigar@users.noreply.github.com>
2023-03-24 02:21:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
-//your code here
-async function getUser() {
-	try {
-		const response = await fetch('https://randomuser.me/api/');
-		const data = await response.json();
-		return data.results[0];
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-function displayUser(user) {
-	document.getElementById('user-img').src = user.picture.large;
-	document.getElementById('user-name').textContent = `${user.name.first} ${user.name.last}`;
-}
-
-
-function displayAdditionalInfo(attribute) {
-  const user = JSON.parse(localStorage.getItem('user'));
-  if (attribute === 'age') {
-    document.getElementById('info-data').textContent = `Age: ${user.dob.age}`;
-  } else if (attribute === 'email') {
-    document.getElementById('info-data').textContent = `Email: ${user.email}`;
-  } else if (attribute === 'phone') {
-    document.getElementById('info-data').textContent = `Phone: ${user.phone}`;
+document.addEventListener('DOMContentLoaded', function() {
+  // your code here
+  async function getUser() {
+    try {
+      const response = await fetch('https://randomuser.me/api/');
+      const data = await response.json();
+      return data.results[0];
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
-
-const getUserBtn = document.getElementById('getUser');
-getUserBtn.addEventListener('click', async () => {
-  const response = await fetch('https://randomuser.me/api/');
-  const data = await response.json();
-  const user = data.results[0];
-  localStorage.setItem('user', JSON.stringify(user));
-  document.getElementById('fullname').textContent = `${user.name.first} ${user.name.last}`;
-  document.getElementById('profile-pic').src = user.picture.large;
-  document.getElementById('info-data').textContent = '';
+  
+  function displayUser(user) {
+    document.getElementById('user-image').src = user.picture.large;
+    document.getElementById('user-name').textContent = `${user.name.first} ${user.name.last}`;
+  }
+  
+  
+  function displayAdditionalInfo(attribute) {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (attribute === 'age') {
+      document.getElementById('info-data').textContent = `Age: ${user.dob.age}`;
+    } else if (attribute === 'email') {
+      document.getElementById('info-data').textContent = `Email: ${user.email}`;
+    } else if (attribute === 'phone') {
+      document.getElementById('info-data').textContent = `Phone: ${user.phone}`;
+    }
+  }
+  console.log(document.getElementById('user-name'));
+  
+  
+  const getUserBtn = document.getElementById('getUser');
+  getUserBtn.addEventListener('click', async () => {
+    const response = await fetch('https://randomuser.me/api/');
+    const data = await response.json();
+    const user = data.results[0];
+    localStorage.setItem('user', JSON.stringify(user));
+    document.getElementById('user-name').textContent = `${user.name.first} ${user.name.last}`;
+    document.getElementById('user-image').src = user.picture.large;
+    document.getElementById('getUser').textContent = 'Get New User';
+  });
+  
+  const buttons = document.querySelectorAll('.additional-info button');
+  buttons.forEach(button => {
+    button.addEventListener('click', event => {
+      const attribute = event.target.getAttribute('data-attr');
+      displayAdditionalInfo(attribute);
+    });
+  });
 });
-
